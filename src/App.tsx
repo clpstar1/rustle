8f4d7406cf73d7a02ce8c33022169159bfc57b78
Steffen Lesch <steffen.lesch96@gmail.com>
2022-09-29 23:08:11 +0200

    rudimentary volume control

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Globals } from './lib/globals';
 import { pianoKeys } from './lib/keymap';
 import { getScale } from './lib/pitch';
 import { Player } from './lib/player';
 import { zip } from './lib/util';
+import VolumeControl from './VolumeControl';
 
 function App() {
 
-  const player = new Player(new AudioContext())
-  const keys = new Map(zip(pianoKeys, getScale()))
 
-  document.addEventListener("keydown", (ev) => {
-    const freq = keys.get(ev.key)
-    if (freq === undefined) return
-    player.play(freq)
-  })
+  const [globals, setGlobals] = useState(new Globals())
 
-  document.addEventListener("keyup", (ev) => {
-    const freq = keys.get(ev.key)
-    if (freq === undefined) return
-    player.stop(freq, "sine")
-  })
+  const [player, _setPlayer] = useState(new Player(globals))
+  const [keys, _setKeys] = useState(new Map(zip(pianoKeys, getScale())))
+
+  useEffect(() => {
+
+    function handleKeyDown(ev: KeyboardEvent) {
+      const freq = keys.get(ev.key)
+      if (freq === undefined) return
+      player.play(freq)
+    }
+
+    function handleKeyUp(ev: KeyboardEvent) {
+      const freq = keys.get(ev.key)
+      if (freq === undefined) return
+      player.stop(freq, "sine")
+    }
+  
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("keyup",  handleKeyUp)
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+      window.removeEventListener("keyup", handleKeyUp)
+    }
+
+  }, [])
+
 
   return (
     <div className="App">
       <h1> Toy-Synth </h1>
+      <VolumeControl setGlobals={setGlobals}/>
     </div>
   );
 }
--- src/lib/player.ts
+++ src/lib/player.ts
@@ -1,8 +1,10 @@
+import { Globals } from "./globals"
+
 class Player {
 
     constructor(
-        private ctx: AudioContext,
-        private oscMap = new Map<string, OscillatorNode | undefined>(),l
+        private ctx: Globals,
+        private oscMap = new Map<string, OscillatorNode | undefined>()
         ) {}
 
     public play = (freq: number, type: OscillatorType = "sine", ) => {
@@ -12,19 +14,20 @@ class Player {
             return 
         }
 
-        const osc = new OscillatorNode(this.ctx, {
+        const osc = new OscillatorNode(this.ctx.audioContext, {
             type: type,
             frequency: freq
         })
 
         this.oscMap.set(k, osc)
 
-        osc.connect(this.ctx.destination)
+        osc.connect(this.ctx.gainNode)
         osc.start()
     } 
     
     public stop = (freq: number, type: OscillatorType) => {
         const k = this.key(freq, type)
+        const osc = this.oscMap.get(k)
         this.oscMap.get(k)?.stop()
         this.oscMap.set(k, undefined)
     }
