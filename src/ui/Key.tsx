e0267304ef71c68cfe9fe10807a825cb371c20a7
steffen <steffen.lesch96@gmail.com>
2025-02-06 01:06:40 +0100

    add some stuffen

--- src/ui/Key.tsx
+++ src/ui/Key.tsx
@@ -1,5 +1,6 @@
 import ArrowUp from "../assets/arrow-up.svg"
 import ArrowDown from "../assets/arrow-down.svg"
+import { useRef, useState } from "react"
 
 interface Props {
     keyboardKey: string
@@ -10,21 +11,46 @@ interface Props {
     mouseup: () => void
 }
 
+const noteColors = {
+    "note": ["#FFFFFF", "#FFBE65"],
+    "unset": ["#FFFFFF", "#CCCCCC"],
+    "octave": ["#FFFFFF", "#72C4DA"]
+}
+
 export default function Key(props: Props) {
+    const delay = 50;
     
-    const onTouchStart = (ev:React.TouchEvent) => { ev.preventDefault(); props.mousedown()}
-    const onTouchEnd = (ev:React.TouchEvent) => { ev.preventDefault(); props.mouseup()}
+    const mousedown = () => { setKeyHeld(true); props.mousedown() }
+    const mouseup = () => { 
+        setTimeout(() => setKeyHeld(false), delay)
+        props.mouseup() }
+    const touchstart = (ev: React.TouchEvent) => { 
+        ev.preventDefault()
+        setKeyHeld(true) 
+        props.mousedown() 
+    }
+    const touchend = (ev: React.TouchEvent) => { 
+        ev.preventDefault()
+        setTimeout(() => setKeyHeld(false), delay)
+        props.mouseup() }
+
+    const [keyHeld, setKeyHeld] = useState(false)
     
-    if (props.type == "note") { return (
+    if (props.type == "note") { 
+        const containerStyle = { backgroundColor: keyHeld ? noteColors['note'][0] : noteColors['note'][1]}
+        const textStyle = { color: keyHeld ? noteColors['note'][1] : noteColors['note'][0]}
+
+        return (
         <div 
             className="mykey-container" 
-            onMouseDown={props.mousedown} 
-            onMouseUp={props.mouseup}
-            onTouchStart={onTouchStart}
-            onTouchEnd={onTouchEnd}
+            onMouseDown={mousedown} 
+            onMouseUp={mouseup}
+            onTouchStart={touchstart}
+            onTouchEnd={touchend}
+            style={containerStyle}
         >
             <div className="mykey-upper">{props.keyboardKey}</div>
-            <div className="mykey-note">{props.note}</div>
+            <div className="mykey-note" style={textStyle}>{props.note}</div>
             {props.sharp && <div className="mykey-sharp">{"#"}</div>}
         </div>
     )
@@ -38,10 +64,10 @@ export default function Key(props: Props) {
         return(
             <div 
                 className="mykey-container-octave"
-                onMouseDown={props.mousedown} 
-                onMouseUp={props.mouseup}
-                onTouchStart={onTouchStart}
-                onTouchEnd={onTouchEnd}
+                onMouseDown={mousedown} 
+                onMouseUp={mouseup}
+                onTouchStart={touchstart}
+                onTouchEnd={touchend}
             >
                 <div className="mykey-arrow">
                     <img src={props.keyboardKey == "Y" ? ArrowDown : ArrowUp}></img>
